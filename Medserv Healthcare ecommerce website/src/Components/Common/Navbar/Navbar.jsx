17e85100d6a60b7b9cf2d5296afa405afb8d3e86
Primani Kaushalya <primani.kaushalya2000@gmail.com>
2024-11-09 15:57:10 +0530

    Combine Product Connecting with Frontend Updates
    MongoDB Connection + Frontend Pages (Blog, Hearts, About, Location)

--- Medserv Healthcare ecommerce website/src/Components/Common/Navbar/Navbar.jsx
+++ Medserv Healthcare ecommerce website/src/Components/Common/Navbar/Navbar.jsx
@@ -6,122 +6,99 @@ import SearchBar from '../Navbar/SearchBar/SearchBar';
 import {Link , useLocation} from 'react-router-dom';
 
 const Navbar = () => {
-  const location = useLocation();
 
-  const getActiveMenu = () => {
-    switch (location.pathname) {
-      case "/":
-        return "home";
-      case "/wellness":
-        return "wellness";
-      case "/medical_devices":
-        return "medicalDevices";
-      case "/personal_care":
-        return "personalCare";
-      case "/hearts":
-        return "hearts";
-      case "/rent":
-        return "rent";
-      case "/blog":
-        return "blog";
-      case "/location":
-        return "location";
-      default:
-        return "none";
-    }
-  };
+    // for changing the state of clicked nav bar item
+    // const [menu, setMenu] = useState("home"); 
+    const location = useLocation();
 
-  const activeMenu = getActiveMenu();
-  const [isClicked, setClicked] = useState(false);
+    const getActiveMenu = () => {
+        switch (location.pathname) {
+          case '/':
+            return 'home';
+          case '/wellness':
+            return 'wellness';
+          case '/medical_devices':
+            return 'medicalDevices';
+          case '/personal_care':
+            return 'personalCare';
+          case '/hearts':
+            return 'hearts';
+          case '/rent':
+            return 'rent';
+          case '/blog':
+            return 'blog';
+          default:
+            return 'none';
+        }
+      };
 
-  const toggleMenu = () => {
-    setClicked((prevClicked) => !prevClicked);
-  };
+    const activeMenu = getActiveMenu();
+    
+    //for displaying the nav bar when clicked span icon
+    const [isClicked, setClicked] = useState(false); 
 
-  const navbarRef = useRef(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navbarRef.current && !navbarRef.current.contains(event.target)) {
-        setClicked(false);
-      }
-      if (event.target.closest(".nav-menu-span")) {
-        setClicked(false);
-      }
+    const toggleMenu = () => {
+        setClicked(prevClicked => !prevClicked);
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    // for dissapear nav bar clicked outside of navbar
+    const navbarRef = useRef(null);
 
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+                // Clicked outside the navbar
+                setClicked(false);
+            }
+            if (event.target.closest('.nav-menu-span')) {
+                // Clicked on the span icon
+                setClicked(false);
+            }
+        };
 
-  return (
-    <div className="navBar">
-      <div className="navBar-cloumn-1">
-        <Link className="nav-bar-link" to="/">
-          <div className="nav-logo">
-            <img src={logo} alt="" />
-          </div>
-        </Link>
-        <ul
-          className={isClicked ? "nav-menu active" : "nav-menu"}
-          ref={navbarRef}
-        >
-          <Link className="nav-bar-link" to="/">
-            <li className={activeMenu === "home" ? "active" : ""}>
-              Home{activeMenu === "home" ? <hr /> : null}
-            </li>
-          </Link>
-          <Link className="nav-bar-link" to="/wellness">
-            <li className={activeMenu === "wellness" ? "active" : ""}>
-              Wellness{activeMenu === "wellness" ? <hr /> : null}
-            </li>
-          </Link>
-          <Link className="nav-bar-link" to="/medical_devices">
-            <li className={activeMenu === "medicalDevices" ? "active" : ""}>
-              Medical Devices{activeMenu === "medicalDevices" ? <hr /> : null}
-            </li>
-          </Link>
-          <Link className="nav-bar-link" to="/personal_care">
-            <li className={activeMenu === "personalCare" ? "active" : ""}>
-              Personal Care{activeMenu === "personalCare" ? <hr /> : null}
-            </li>
-          </Link>
-          <Link className="nav-bar-link" to="/rent">
-            <li className={activeMenu === "rent" ? "active" : ""}>
-              Rent{activeMenu === "rent" ? <hr /> : null}
-            </li>
-          </Link>
-          <Link className="nav-bar-link" to="/blog">
-            <li className={activeMenu === "blog" ? "active" : ""}>
-              Blog{activeMenu === "blog" ? <hr /> : null}
-            </li>
-          </Link>
-          <Link className="nav-bar-link" to="/hearts">
-            <li className={activeMenu === "hearts" ? "active" : ""}>
-              Hearts{activeMenu === "hearts" ? <hr /> : null}
-            </li>
-          </Link>
-        </ul>
-        <div className="nav-signin">
-          <Link className="nav-bar-link" to="/sign_in">
-            <FaUser className="nav-icons" />
-            <p>Sign In</p>
-          </Link>
-        </div>
-        <div className="nav-menu-span" onClick={toggleMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className='navBar'>
+            <div className='navBar-cloumn-1'>
+                <Link className='nav-bar-link' to='/'>
+                    <div className='nav-logo'>
+                        <img src={logo} alt=''/>
+                    </div>
+                </Link>
+                <ul className={isClicked ? 'nav-menu active' : 'nav-menu'} ref={navbarRef}>
+                    <Link className='nav-bar-link' to='/'><li className={activeMenu === "home" ? "active" : ""}>Home{activeMenu === "home" ? <hr /> : null}</li></Link>
+                    <Link className='nav-bar-link' to='/wellness'><li className={activeMenu === "wellness" ? "active" : ""}>Wellness{activeMenu === "wellness" ? <hr /> : null}</li></Link>
+                    <Link className='nav-bar-link' to='/medical_devices'><li className={activeMenu === "medicalDevices" ? "active" : ""}>Medical Devices{activeMenu === "medicalDevices" ? <hr /> : null}</li></Link>
+                    <Link className='nav-bar-link' to='/personal_care'><li className={activeMenu === "personalCare" ? "active" : ""}>Personal Care{activeMenu === "personalCare" ? <hr /> : null}</li></Link>
+                    <Link className='nav-bar-link' to='/hearts'><li className={activeMenu === "hearts" ? "active" : ""}>Hearts{activeMenu === "hearts" ? <hr /> : null}</li></Link>
+                    <Link className='nav-bar-link' to='/rent'><li className={activeMenu === "rent" ? "active" : ""}>Rent{activeMenu === "rent" ? <hr /> : null}</li></Link>
+                    <Link className='nav-bar-link' to='/blog'><li className={activeMenu === "blog" ? "active" : ""}>Blog{activeMenu === "blog" ? <hr /> : null}</li></Link>
+                </ul>
+                <div className='nav-signin'>
+                    <Link className='nav-bar-link' to='/sign_in'>
+                        <FaUser className='nav-icons'/>
+                        <p>Sign In</p>
+                    </Link>
+                </div>
+                <div className='nav-menu-span' onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
 
             <div className='navBar-cloumn-2'>
-                <div className='navBar-search'>
+                {/* <div className='navBar-search'>
                     <input type="text" placeholder='Search..'/>
-                </div>
+                </div>  */}
+                <SearchBar className='navBar-search'/>
+
                 <div className='nav-upload-prescriptions'>
                     <Link className='nav-bar-link' to='/upload_prescriptions'>
                         <button>
@@ -142,4 +119,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default Navbar
