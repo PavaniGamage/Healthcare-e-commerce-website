301ce72defb4db81c8e6937c7f7d88b04ef2ef79
Primani Kaushalya <primani.kaushalya2000@gmail.com>
2024-11-09 16:53:53 +0530

    Update .env.example

--- Backend - Medserv/index.js
+++ Backend - Medserv/index.js
@@ -7,8 +7,9 @@ require("dotenv").config(); // Load .env file
 // import dependencies and modules
 const express = require("express");
 const mongoose = require("mongoose");
-const dbConnect = require("./dbConnect");             
-// const productRoutes = require("./routes/products");
+const dbConnect = require("./dbConnect");    
+const Product = require("./models/Product");         
+const productRoutes = require("./routes/products");
 const cors = require("cors");
 const app = express();
 
@@ -48,130 +49,8 @@ app.get("/", (req, res) => {
     res.send("Hello, MongoDB!, Express App is Running");
 });
 
-//---------on change------------------
-
-// Schema for Creating Products
-const Product = mongoose.model("Product", {
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    availability: {
-        type: String,
-        enum: ["Available", "Not Available"],
-        required: true
-    },
-    description: {
-        type: String,
-        default: "-"
-    },
-    subDescription: {
-        type: String,
-        default: "-"
-    },
-    keywords: {
-        type: String, // Can also be an array 
-        default: "-"
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 5,
-        default: 0
-    },
-    imageSource: {
-        type: String,
-        enum: ["url", "file"],
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        default: "-"
-    },
-    category1: {
-        type: String,
-        default: "-"
-    },
-    category2: {
-        type: String,
-        default: "-"
-    },
-    itemType: {
-        type: String,
-        enum: ["Rent", "Sell"],
-        required: true
-    },
-    dailyRental: {
-        type: Number,
-        default: 0
-    },
-    weeklyRental: {
-        type: Number,
-        default: 0
-    },
-    monthlyRental: {
-        type: Number,
-        default: 0
-    },
-    deposit: {
-        type: Number,
-        default: 0
-    },
-    createdAt: {
-        type: Date,
-        // required: true
-        default: Date.now()
-    },
-    updatedAt: {
-        type: Date,
-        // required: true
-        default: Date.now()
-    }
-}, "medserv_products"); // the collection name
-
-// create endpoint for products, send to frontend
-app.get('/formattedProducts', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        console.log("Fetched Products:", products); // Log the fetched products
-
-        if (products.length === 0) {
-            return res.status(404).send("No products found");
-        }
-
-        const formattedProducts = products.map(product => ({
-            itemId: product._id,
-            itemType: product.itemType,
-            name: product.name,
-            price: product.price.toFixed(2),
-            availability: product.availability,
-            description: product.description,
-            subDescription: product.subDescription, 
-            imageSource: product.imageSource,
-            imageUrl: product.imageUrl,
-            categoryMain: product.category1,
-            categorySub: product.category2,
-            keywords: product.keywords,
-            rating: product.rating,
-            daillyRental: product.dailyRental.toFixed(2),
-            weeklyRental: product.weeklyRental.toFixed(2),
-            monthlyRental: product.monthlyRental.toFixed(2),
-            deposit: product.deposit.toFixed(2)
-        }));
-
-        res.status(200).json(formattedProducts);
-    } catch (error) {
-        console.error("Error fetching formatted products:", error);
-        res.status(500).send("Error fetching formatted products");
-    }
-});
-
-// // for getting products
-// app.use("/formattedProducts", productRoutes);
+// for getting products
+app.use("/formattedProducts", productRoutes);
 
 // Starting the server
 app.listen(port, (error) => {
